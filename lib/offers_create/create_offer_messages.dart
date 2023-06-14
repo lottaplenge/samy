c2403ec2507f1c9d4720f7147f475ee1e3769e73
LottaPlenge <lotta.plenge@studserv.uni-leipzig.de>
2023-06-14 16:37:02 +0200

    preparations for backend-communnications

--- lib/offers_create/create_offer_messages.dart
+++ lib/offers_create/create_offer_messages.dart
@@ -1,15 +1,18 @@
 import 'package:bloc_mvu_app/mvu/messaging.dart';
 import 'package:bloc_mvu_app/offers_create/create_offer_model.dart';
 import 'package:bloc_mvu_app/offers_create/create_offer_update.dart';
+import 'package:bloc_mvu_app/user/user_message.dart';
+import '../user/user_model.dart';
 
 abstract class CreateOfferMessage extends UpdateMessage {
   CreateOfferMessage() : super(routeTo: CreateOfferUpdate);
 }
 
-class CreateOffer extends CreateOfferMessage {
-  CreateOffer({required this.schoolType, required this.classNo, required this.schoolName});
+class CreateOffer extends CreateOfferMessage { //implements createUser? mit @Override
+  CreateOffer({required this.classNumber, required this.firstSchoolday, required this.schoolId});
 
-  final SchoolType schoolType;
-  final int classNo;
-  final SchoolName schoolName;
+  final int classNumber;
+  final String firstSchoolday;
+  final String schoolId;
+
 }
--- lib/offers_create/create_offer_model.dart
+++ lib/offers_create/create_offer_model.dart
@@ -1,3 +1,6 @@
+import 'package:bloc_mvu_app/user/user_model.dart';
+import 'package:dartz/dartz.dart';
+
 class CreateOfferModel {
   CreateOfferModel();
 
@@ -5,14 +8,42 @@ class CreateOfferModel {
 
   CreateOfferModel copyWith() =>
       CreateOfferModel(
-
       );
 }
 
 final CreateOfferModel initialCreateOffersModel = CreateOfferModel(
 
 );
+class Offer {
+  Offer({
+    required this.offerId,
+    required this.schoolId,
+    required this.firstSchoolDay,
+    required this.classNumber,
+  });
 
-// ignore: constant_identifier_names
-enum SchoolType { Gymnasium, Realschule, Hauptschule, Grundschule, Berufsschule }
-enum SchoolName {Schule1, Schule2, Schule3}
+  factory Offer.fromJSON(Map<String, dynamic> data) {
+    // note the explicit cast to String
+    // this is required if robust lint rules are enabled
+    final offerId = data ['offerId'] as String;
+    final schoolId = data['schoolId'] as String;
+    final classNumber = data['classNumber'] as int;
+    final firstSchoolDay = data['firstSchoolDay'] as String;
+
+    return Offer(
+      offerId: offerId,
+      schoolId: schoolId,
+      classNumber: classNumber,
+      firstSchoolDay: firstSchoolDay,
+
+    );
+  }
+
+  final String offerId, firstSchoolDay, schoolId;
+  final int classNumber;
+
+}
+
+  // ignore: constant_identifier_names
+  enum SchoolType { Gymnasium, Realschule, Hauptschule, Grundschule, Berufsschule }
+  enum SchoolName {Schule1, Schule2, Schule3}
